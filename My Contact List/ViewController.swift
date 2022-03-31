233cb3c93e80d968aac156fa8a54a5a170645972
ric <eroche1@student.gsu.edu>
2022-03-30 21:20:44 -0400

    Initial Commit

---
+++ My Contact List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My Contact List
+//
+//  Created by E Roche on 3/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
