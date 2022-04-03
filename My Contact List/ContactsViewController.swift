890893a91eae27319e8a2695369086c0afee1f7e
ric <eroche1@student.gsu.edu>
2022-04-03 19:16:11 -0400

    added stack view to scroll view - added all keyboard functions

--- My Contact List/ContactsViewController.swift
+++ My Contact List/ContactsViewController.swift
@@ -9,11 +9,68 @@ import UIKit
 
 class ContactsViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.registerKeyboardNotifications()
+        //When the scene is about to be displayed, a method is called to register the code to listen for notifications that the keyboard has been displayed.
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.unregisterKeyboardNotifications()
+        //when the view disappears, a method is called to stop the keyboard from listening for notifications.
+    }
+    
+    //registers the code for notifications and tells the system to execute the appropriate method when the event occurs.
+    func registerKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(ContactsViewController.keyboardDidShow(notification:)), name: UIResponder.keyboardDidShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(ContactsViewController.keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    //removes the listener
+    func unregisterKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    //Information is collected from the notification to get the size of the keyboard displayed.
+    //This is needed to move the content the appropriate amount.
+    @objc func keyboardDidShow(notification: NSNotification) {
+        let userInfo: NSDictionary = notification.userInfo! as NSDictionary
+        let keyboardInfo = userInfo[UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue
+        let keyboardSize = keyboardInfo.cgRectValue.size
+        
+        //get the existing contentInset for the scrollView and set the bottom property to be the height of the keyboard
+        //Content Insets are the distance of the Scroll View’s content from the Scroll View’s edges. The insets collection is retrieved, and the bottom inset is set to the height of the keyboard so that the content will be above the keyboard.
+        var contentInset = self.scrollView.contentInset
+        contentInset.bottom = keyboardSize.height
+        
+        //The Scroll View is then set to use the new content insert values.
+        self.scrollView.contentInset = contentInset
+        self.scrollView.scrollIndicatorInsets = contentInset
+    }
+    
+    //When the keyboard disappears, the Scroll View’s content insert values are set back to the original values.
+    @objc func keyboardWillHide(notification: NSNotification) {
+        var contentInset = self.scrollView.contentInset
+        contentInset.bottom = 0
+        self.scrollView.contentInset = contentInset
+        self.scrollView.scrollIndicatorInsets = UIEdgeInsets.zero
+    }
+    
+    
+    
+    
+    
+    
     
 
     /*
