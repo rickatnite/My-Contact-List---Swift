8f7383f1e0a3b62df9be0a81afda8e329311fd2c
ric <eroche1@student.gsu.edu>
2022-04-12 13:16:35 -0400

    able to select and store settings - fixed picker view issue

--- My Contact List/SettingsViewController.swift
+++ My Contact List/SettingsViewController.swift
@@ -12,7 +12,7 @@ class SettingsViewController: UIViewController, UIPickerViewDataSource, UIPicker
     @IBOutlet weak var swAscending: UISwitch!
 
     //array to store the items that will show up in the Picker View
-    let sortOrderItems: Array<String> = ["ContactName", "City", "Birthday"]
+    let sortOrderItems: Array<String> = ["Contact Name", "City", "Birthday"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,8 +25,36 @@ class SettingsViewController: UIViewController, UIPickerViewDataSource, UIPicker
         pckSortField.delegate = self;
     }
     
-    @IBAction func sortDirectionChanged(_ sender: Any) {
+    override func viewWillAppear(_ animated: Bool) {
+        //set the UI based on the values in userDefaults
+        let settings = UserDefaults.standard
+        //sets the value of the Switch based on the value in the sortDirectionAscending key by calling the setOn: method on the Switch
+        swAscending.setOn(settings.bool(forKey: "sortDirectionAscending"), animated: true)
+        //read the sortField value into a constant
+        let sortField = settings.string(forKey: "sortField")
+        var i = 0
+        //the Picker View is updated by telling it which number row to select, so the for loop goes through the sortOrderItems array
+        for field in sortOrderItems {
+            if field == sortField {
+                //if a match is found, the Picker View is told to select that row
+                //set to zero bc there is only one component.
+                //animated to false bc the selection is already made before the user opens the Selection screen
+                pckSortField.selectRow(i, inComponent: 0, animated: false)
+            }
+            i += 1
+        }
+        pckSortField.reloadComponent(0) //call to have the Picker View change
     }
+    
+    
+    //store the value of the switch
+    @IBAction func sortDirectionChanged(_ sender: Any) {
+        let settings = UserDefaults.standard
+        settings.set(swAscending.isOn, forKey: "sortDirectionAscending")
+        settings.synchronize()
+    }
+    
+    
     
     // MARK: UIPickerViewDelegate Methods
     
@@ -35,22 +63,31 @@ class SettingsViewController: UIViewController, UIPickerViewDataSource, UIPicker
         return 1
     }
     
+    
+    
     //returns the number of row in the picker
-    func pickerView(_ _pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return sortOrderItems.count
     }
+    
+    
     
     //sets the value that is shown for each row in the picker
     //When the Picker is displayed, the system will make repeated calls to this method
     //passing in the row number and getting the corresponding text for the row back.
     //In this case, the method uses the row number to return the corresponding item from the array.
-    func pickerView(_pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return sortOrderItems[row]
     }
     
-    //if user chooses from the pickerview it calls this function
-    func pickerView(_pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        
+    
+    
+    //if user chooses from the pickerview it calls this function - saves userr settings
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let sortField = sortOrderItems[row]
+        let settings = UserDefaults.standard
+        settings.set(sortField, forKey: "sortField")
+        settings.synchronize()
         print("Chosen item: \(sortOrderItems[row])")
     }
     
